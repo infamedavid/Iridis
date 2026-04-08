55019de7ce1f26dd0b30ca5e8fa40cddaf0b141f
david <effetagroove@gmail.com>
2026-04-08 20:08:31 +0700

    Delete assets directory

--- assets/processing/__init__.py
+++
@@ -1,5 +0,0 @@
-from .diffuse import generate_albedo_map
-from .roughness import generate_roughness_map
-from .metallic import generate_metallic_map
-from .normal import generate_normal_map
-from .height import generate_height_map
